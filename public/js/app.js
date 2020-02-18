a28825f50fa4be2bd58e790f36462cec50f5e09b
ppetitea <pierre.petiteau.4985@gmail.com>
2020-02-18 20:17:43 +0100

    fix result display when server provide error

--- public/js/app.js
+++ public/js/app.js
@@ -5,15 +5,17 @@ const forecastLogo = document.getElementById('forecast-logo')
 form.addEventListener('submit', (e) => {
 	e.preventDefault()
 		const message = document.getElementById('message')
+		const location = document.getElementById('location')
+		const forecast = document.getElementById('forecast')
 		message.innerHTML = 'Loading...'
 		fetch('/weather?address=' + search.value).then((response) => {
 		response.json().then((data) => {
 			if (data.error) {
 				message.innerHTML = data.error;
 				forecastLogo.style.display = 'none';
+				location.style.display = 'none';
+				forecast.style.display = 'none';
 			} else {
-				const location = document.getElementById('location')
-				const forecast = document.getElementById('forecast')
 				message.innerHTML = ''
 				forecastLogo.style.display = 'flex';
 				forecastLogo.src = '/img/' + data.weatherIcon
