b6b81ba80e69ed24468db9babc8706d1e03ba17a
ppetitea <pierre.petiteau.4985@gmail.com>
2020-02-18 20:41:14 +0100

    fix result display when server provide error

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,34 @@
 const form = document.querySelector('form')
 const search = document.querySelector('input')
-const forecastLogo = document.getElementById('forecast-logo')
+
+const display_forecast = (message = '', location = '', summary = '', iconPath = null) => {
+	const messageEl = document.getElementById('message')
+	const locationEl = document.getElementById('location')
+	const summaryEl = document.getElementById('forecast')
+	const weatherLogo = document.getElementById('forecast-logo')
+
+	messageEl.innerHTML = message
+	locationEl.innerHTML = location
+	summaryEl.innerHTML = summary
+	if (iconPath) {
+		weatherLogo.style.display = 'flex'
+		weatherLogo.src = iconPath
+	} else {
+		weatherLogo.style.display = 'none'
+	}
+}
 
 form.addEventListener('submit', (e) => {
 	e.preventDefault()
-		const message = document.getElementById('message')
-		const location = document.getElementById('location')
-		const forecast = document.getElementById('forecast')
 		message.innerHTML = 'Loading...'
 		fetch('/weather?address=' + search.value).then((response) => {
 		response.json().then((data) => {
 			if (data.error) {
-				message.innerHTML = data.error;
-				forecastLogo.style.display = 'none';
-				location.style.display = 'none';
-				forecast.style.display = 'none';
+				display_forecast(data.error)
 			} else {
-				message.innerHTML = ''
-				forecastLogo.style.display = 'flex';
-				forecastLogo.src = '/img/' + data.weatherIcon
-				location.innerHTML = data.address;
-				forecast.innerHTML = data.summary + ' ' + data.temperature;
+				display_forecast('', data.address,
+					data.summary + ' ' + data.temperature,
+					'/img/' + data.weatherIcon)
 			}
 		})
 	})
